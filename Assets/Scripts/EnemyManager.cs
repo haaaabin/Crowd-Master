09ecf0ca96af78a35864b2030ee0146797ad57f1
choihabin <chabin7181@gmail.com>
2025-02-19 21:21:41 +0900

    Update: EnemyManager.cs

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -6,15 +6,13 @@ using Random = UnityEngine.Random;
 public class EnemyManager : MonoBehaviour
 {
     public TextMeshPro counterTxt;
-    [SerializeField] private GameObject stickMan;
-
-    [Range(0f, 1f)][SerializeField] private float distanceFactor, radius;
-
     public Transform enemy;
+    [SerializeField] private GameObject stickMan;
+    [Range(0f, 1f)][SerializeField] private float distanceFactor, radius;
     public bool isAttack;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         for (int i = 0; i < Random.Range(20, 120); i++)
         {
@@ -22,33 +20,35 @@ public class EnemyManager : MonoBehaviour
         }
 
         counterTxt.text = transform.childCount.ToString();
-
         FormatStickMan();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (isAttack && transform.childCount > 1)
         {
-            var enemyDirection = enemy.position - transform.position;
+            HandleAttack();
+        }
+    }
 
-            for (int i = 0; i < transform.childCount; i++)
+    private void HandleAttack()
+    {
+        var enemyDirection = enemy.position - transform.position;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).rotation = Quaternion.Slerp(transform.GetChild(i).rotation, quaternion.LookRotation(enemyDirection, Vector3.up), Time.deltaTime * 3f);
+
+            if (enemy.childCount > 0)
             {
-                transform.GetChild(i).rotation = Quaternion.Slerp(transform.GetChild(i).rotation, quaternion.LookRotation(enemyDirection, Vector3.up),
-                    Time.deltaTime * 3f);
+                var distance = enemy.GetChild(1).position - transform.GetChild(i).position;
 
-                if (enemy.childCount > 1)
+                if (distance.magnitude < 4f)
                 {
-                    var distance = enemy.GetChild(1).position - transform.GetChild(i).position;
-
-                    if (distance.magnitude < 5f)
-                    {
-                        transform.GetChild(i).position = Vector3.Lerp(transform.GetChild(i).position, enemy.GetChild(1).position, Time.deltaTime * 2f);
-                    }
+                    transform.GetChild(i).position = Vector3.Lerp(transform.GetChild(i).position, enemy.GetChild(1).position, Time.deltaTime);
                 }
             }
-
         }
     }
 
@@ -65,7 +65,7 @@ public class EnemyManager : MonoBehaviour
         }
     }
 
-    public void Attack(Transform enemyForce)
+    public void Attacking(Transform enemyForce)
     {
         enemy = enemyForce;
         isAttack = true;
